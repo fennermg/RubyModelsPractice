533cc27934ed3a5369a89fea1a815a5e0e346c44
fennermg <fmenagaray@gmail.com>
2021-11-19 12:42:13 -0600

    basic User validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+    validates :name, :lastName, :birthDate, :password, :active, :email, presence: true
+    validates :password , length: { minimum: 3 }
+
+    validates :email,   format: { with: URI::MailTo::EMAIL_REGEXP, message: "Email invalid"  },
+                        uniqueness: { case_sensitive: false },
+                        length: { minimum: 4, maximum: 254 }
+
 end
--- db/migrate/20211118171839_create_users.rb
+++ db/migrate/20211118171839_create_users.rb
@@ -1,14 +1,14 @@
 class CreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
-      t.string :name
-      t.string :lastName
-      t.date :birthDate
-      t.string :password
-      t.boolean :active
-      t.string :email
-
+      t.string :name, null: false
+      t.string :lastName, null: false
+      t.date :birthDate, null: false
+      t.string :password, null: false
+      t.boolean :active, null: false, default: true
+      t.string :email, null: false
       t.timestamps
     end
+
   end
 end
