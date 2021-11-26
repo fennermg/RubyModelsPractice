39b2ff675f703d4c85d8b8b854d919c6d8a91219
fennermg <fmenagaray@gmail.com>
2021-11-26 11:18:47 -0600

    Product validations added

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,22 @@
 class Product < ApplicationRecord
+
+    before_validation :generate_productcode
+
+    validates :name, :code, :price, :quantity, presence: true
+
+    validates :active, inclusion: [true, false]
+    validates :active, exclusion: [nil]
+
+    validates :price, numericality: { greater_than: 0.0 }
+
+
+    def generate_productcode
+        self.code= SecureRandom.random_number(100000000);
+    end
+
+    def deactivate
+        self.update(active: false)
+    end
+
     has_many :order_items
 end
